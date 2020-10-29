a0d6a653c760cccc1d78421d0582b46da961735b
doj <flyingdoji@protonmail.com>
2020-10-29 19:03:45 +0200

    whois command line tool

---
+++ cmd/whois/whois.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"whois"
+)
+
+func main() {
+
+	if len(os.Args) != 2 {
+		fmt.Fprintln(os.Stdout, "Usage:\twhois <domain name>")
+		return
+	}
+	if s, r, err := whois.Whois(os.Args[1]); err != nil {
+		fmt.Fprintln(os.Stdout, err)
+	} else {
+		fmt.Fprintln(os.Stdout, "Queried ", s)
+		fmt.Fprintln(os.Stdout, r)
+	}
+}
